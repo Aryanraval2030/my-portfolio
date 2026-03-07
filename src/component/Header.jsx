0d30aa10c30b9f2b5d3002ff6951895fbbbd28a9
Aryanraval2030 <aryanraval1432@gmail.com>
2026-03-07 11:14:26 +0530

    updated

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 function Header() {
   return (
-    <header className="bg-[#161b22] h-[14vh] bg-[#161b22]/30 backdrop-blur-md flex justify-center gap-[40rem] text-xl items-center text-[#58a6ff] fixed z-10 top-0 w-[100%]">
+    <header className="bg-[#161b22] h-[14vh] bg-[#161b22]/30 backdrop-blur-md flex justify-center gap-[40rem] text-xl items-center text-[#58a6ff] fixed top-0 w-[100%]">
       <div>
         <h1 className="text-[26px]">AR</h1>
       </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
-    <div className='h-[100vh] border-2 mt-[15vh]'>
-      <h1 className="mb-[5rem] text-4xl text-[#64ffda] text-center ">contact me</h1>
+    <div className="h-[100vh] border-2 mt-[15vh]">
+      <h1 className="mb-[5rem] text-4xl text-[#64ffda] text-center ">
+        contact me
+      </h1>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
